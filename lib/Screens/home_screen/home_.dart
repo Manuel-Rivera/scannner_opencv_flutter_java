e668cb6e463f9c248ee92727ddafa2cb758fc51c
JesusMacedonio <97920434+JesusMacedonio@users.noreply.github.com>
2023-03-13 15:55:51 -0600

    Fix: screenhome

--- lib/Screens/home_screen/home_.dart
+++ lib/Screens/home_screen/home_.dart
@@ -1,17 +1,26 @@
 import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_exif_rotation/flutter_exif_rotation.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:printing/printing.dart';
 import 'package:provider/provider.dart';
+import 'package:tuple/tuple.dart';
+import 'package:xml/xml.dart';
 
+import '../../Providers/login_provider.dart';
 import '../../Model/docuement_model.dart';
 import '../../Providers/document_provider.dart';
+import '../../widgets/widgets.dart';
+import '../login_screen/login.dart';
 import '../scanner_screen/drawer.dart';
 import '../scanner_screen/new_image.dart';
 import '../scanner_screen/pdf_screen.dart';
 import 'search.dart';
+
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -104,6 +113,7 @@ class Home extends StatelessWidget {
   //!DOCUMENT CARD
   Widget _documentCard(BuildContext context, DocumentModel document,
       Animation<double> animation) {
+        bool enviando2 = false;
     return SizeTransition(
       sizeFactor: animation,
       child: Card(
@@ -191,8 +201,21 @@ class Home extends StatelessWidget {
                           Icons.cloud_upload,
                           color: ThemeData.dark().colorScheme.secondary,
                         ),
-                        onPressed: () {
-                          //TODO: CLOUD UPLOAD IMAGE
+                        onPressed: () async {
+                          String? result = await formularioEnvio(context);
+                          if(result != null && result.compareTo("cancel") != 0){
+                            //Evita el envio multiple del mismo archivo si ya esta enviandose 
+                            if (!enviando2){ 
+                              enviando2 = true;
+                              var documentPath = document.pdfPath;        
+                              sendFile(context,documentPath).then((tuple) {
+                                //int arriva = tuple.item1;
+                                String mensaje = tuple.item2;
+                                enviando2 = false;
+                                alertaDocumentoSubido(context,mensaje);
+                              });
+                            }
+                          }
                         },
                       ),
                       IconButton(
@@ -207,7 +230,20 @@ class Home extends StatelessWidget {
                           })
                     ],
                   ),
-                )
+                ),
+                //Construye la animacion de cargando (O no) cuando sea necesario
+                
+                StatefulBuilder(
+                builder: (BuildContext context, StateSetter setState) {
+
+                  return !enviando2?TextButton(onPressed:(){
+                    enviando2=!enviando2;
+                    print("*******$enviando2");
+                    setState(){};
+                  }, child: Text("HOLA")):CircularProgressIndicator();
+                },
+              ),
+                      
               ],
             )
           ],
@@ -430,4 +466,88 @@ class Home extends StatelessWidget {
     //!SE REGRESA A LA PANTALL ANTERIOR
     Navigator.of(context).pop();
   }
+
+  //Envia una peticion para obtener el nombre del archivo a subir (numero consecutivo)
+Future<String> numeroArchivo(BuildContext context) async {
+  var request = http.MultipartRequest(
+    'POST',
+    Uri.parse('http://192.168.56.1:8080/siia/ldXML'),
+  );
+
+  // Parametros 
+  request.fields['lista'] = 'lista';
+  request.fields['cuso'] = 'comun.cntArchis';
+  
+  // Set the session ID as a cookie in the request headers
+  request.headers['cookie'] = 'JSESSIONID='+context.read<loginProvider>().obtener_idSesion();
+  // Send the request and get the response
+  var response = await request.send();
+  var responseBody = await response.stream.bytesToString();
+  
+  //La respuesta viene en un HTML asi que se convierte en un XmlDocument
+  final document = XmlDocument.parse(responseBody);
+  final cntElement = document.findAllElements('CNT').first;
+  final cntValue = cntElement.text;
+  return cntValue;  
+}
+
+
+ Future <Tuple2<int, String>> sendFile(BuildContext context, String path) async {
+  var request = http.MultipartRequest(
+    'POST',
+    Uri.parse('http://192.168.56.1:8080/siia/carPDF2'),
+  );
+
+  // Add the file parameter to the request
+  var file = File(path);
+  var stream = http.ByteStream(file.openRead());
+  var length = await file.length();
+  var multipartFile = http.MultipartFile('archi', stream, length,
+      filename: 'example.pdf');
+
+  request.files.add(multipartFile);
+
+  //Secuencia del archivo a subir
+  String numero = await numeroArchivo(context);
+
+  // Add other parameters to the request
+  request.fields['usr'] = context.read<loginProvider>().obtener_usuario();
+  request.fields['num'] = numero;             //Numero de la secuencia que le corresponde al archivo
+  request.fields['dir'] = 'archivos';         //Directorio donde se guardara el arhivo
+  request.fields['id'] = numero;              //Id del archivo
+  request.fields['coments'] = 'Desde flutter';//Comentarios 
+  request.fields['arch_alumno'] = '0617386J'; //Matricula del alumno que le corresponde el archivo
+  request.fields['arch_nombre'] = 'NOMBRE';   //Nombre real del archivo
+  request.fields['arch_ctype'] = 'PDF';       //Tipo de archivo
+  request.fields['arch_size'] = '345684';     //Tamaño del archivo
+  request.fields['arch_tdoc'] = '115';        //Tipo de documento
+  request.fields['arch_boveda'] = '1';        //Identificador en la boveda
+  request.fields['arch_wid'] = '';            //Identificador ascendente para el siia web           (OPCIONAL)
+  request.fields['arch_warchid'] = '';        //Identificador archivo siia web                      (OPCIONAL)     
+  request.fields['arch_comen'] = '';          //Comentarios                                         (OPCIONAL)     
+
+  
+
+  // Set the session ID as a cookie in the request headers
+  request.headers['cookie'] = 'JSESSIONID='+context.read<loginProvider>().obtener_idSesion();
+
+  // Send the request and get the response
+  var response = await request.send();
+  var responseBody = await response.stream.bytesToString();
+  var jsonResponse = json.decode(responseBody);
+  print(context.read<loginProvider>().obtener_usuario());
+  print(jsonResponse);
+
+  if (response.statusCode == 200) {
+    if(jsonResponse.containsKey('ERROR')){
+        return Tuple2(0, jsonResponse["ERROR"]);
+      }
+      if(jsonResponse.containsKey('OK')){
+        return Tuple2(1, "OK");
+      }
+  }
+  return Tuple2(0, response.statusCode.toString());
+}
+
+
 }
