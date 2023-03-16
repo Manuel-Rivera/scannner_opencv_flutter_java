b02fa940f6a7115f532a1c191b72aa20f3c07f0e
JesusMacedonio <97920434+JesusMacedonio@users.noreply.github.com>
2023-03-16 08:20:17 -0600

    Obtencion de los tipos de documentos en formulario de envio

--- lib/widgets/dropdowns.dart
+++ lib/widgets/dropdowns.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tuple/tuple.dart';
+import 'package:http/http.dart' as http;
+
+import '../Providers/login_provider.dart';
 
 List<DropdownMenuItem<String>> get dropdownItems {
   List<DropdownMenuItem<String>> menuItems = [
@@ -15,6 +20,44 @@ List<DropdownMenuItem<String>> get dropdownItems {
         value: "103", child: Text("TCONSTANCIA DE TERM. DE MAEST.")),
   ];
   return menuItems;
+}
+
+//Envia una peticion para obtener los tipos de documentos que maneja la base de datos
+  Future<String> stringTdocum(BuildContext context) async {
+    var request = http.MultipartRequest(
+      'POST',
+      Uri.parse('http://148.216.31.181:8080/siia/getPTDOCEMP'),
+    );
+
+    // Set the session ID as a cookie in the request headers
+    request.headers['cookie'] =
+        'JSESSIONID=${Provider.of<LoginProvider>(context, listen: false)}';
+    try {
+      // Send the request and get the response
+      var response = await request.send();
+      var responseBody = await response.stream.bytesToString();
+      //var responseBody = "";
+      
+      return responseBody;
+    } catch (e) {
+      return "";
+    }
+  }
+
+
+//Funcion que crea una lista de DropdownMenuItem en base a una peticion POST a la base de datos
+Future<List<DropdownMenuItem<String>>> StringToDropdownlist(BuildContext context) async{
+  String tdocum = await stringTdocum(context);
+  List<String> parts = tdocum.replaceAll("{", "").replaceAll("}", "").split(", ");
+  List<DropdownMenuItem<String>> dropdownItems = parts.map((part) {
+  List<String> pair = part.split("=");
+  return DropdownMenuItem(
+      value: pair[0],
+      child: Text(pair[1]),
+    );
+  }).toList();
+
+  return dropdownItems;
 }
 
 class DropdownButtonExample extends StatefulWidget {
@@ -34,26 +77,36 @@ class _DropdownButtonExampleState extends State<DropdownButtonExample> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      hint: const Text("Tipo de documento"),
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_drop_down),
-      elevation: 16,
-      style: const TextStyle(color: Colors.black),
-      underline: Container(
-        height: 2,
-        color: Colors.black,
-      ),
-      onChanged: (String? value) {
-        // This is called when the user selects an item.
-        setState(() {
-          dropdownValue = value!;
-        });
-        //Llamada a la funcion callback pasandole como parametro el valor seleccionado
-        widget.onSelectedValueChanged(
-            dropdownValue!); // Call the callback function with the selected value
+    return FutureBuilder<List<DropdownMenuItem<String>>>(
+      future: StringToDropdownlist(context),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return DropdownButton<String>(
+            hint: const Text("Tipo de documento"),
+            value: dropdownValue,
+            icon: const Icon(Icons.arrow_drop_down),
+            elevation: 16,
+            style: const TextStyle(color: Colors.black),
+            underline: Container(
+              height: 2,
+              color: Colors.black,
+            ),
+            onChanged: (String? value) {
+              // This is called when the user selects an item.
+              setState(() {
+                dropdownValue = value!;
+              });
+              //Llamada a la funcion callback pasandole como parametro el valor seleccionado
+              widget.onSelectedValueChanged(
+                  dropdownValue!); // Call the callback function with the selected value
+            },
+            items: snapshot.data!,
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        return const CircularProgressIndicator();
       },
-      items: dropdownItems,
     );
   }
 }
